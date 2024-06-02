4822cdebe36cf68e230138236b03a96ee098bd07
jesslee2023 <jingping.li@hyperisland.se>
2024-06-02 13:11:30 +0200

    fix: 3d

--- src/3D.js
+++ src/3D.js
@@ -1,66 +1,64 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.149.0/build/three.module.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.149.0/examples/jsm/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x0B2447);
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const texture = new THREE.TextureLoader().load("SaveWater.png")
-texture.wrapS = THREE.RepeatWrapping;
-texture.wrapT = THREE.RepeatWrapping;
-texture.repeat.set( 6 , 6 );
+scene.background = new THREE.Color(0x0b2447);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize(800, 600);
+const container = document.getElementById("three-container");
+container.appendChild(renderer.domElement);
 
-const controls = new OrbitControls( camera, renderer.domElement );
+camera.aspect = 800 / 600;
+camera.updateProjectionMatrix();
 
-camera.position.set( 0, 0, 5 );
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableZoom = false;
+
+camera.position.set(0, 0, 15);
 controls.update();
 
-//const geometry = new THREE.BoxGeometry(2,2,2);
-const geometry = new THREE.SphereGeometry( 5, 64, 32, 0 , 6.3, 0, 6.3); 
-//const geometry = new THREE.CircleGeometry( 5, 32 ); 
-const material = new THREE.MeshLambertMaterial({ map: texture});
+const light = new THREE.AmbientLight("white", 2); // Soft white light
+scene.add(light);
 
-const cube = new THREE.Mesh(geometry, material);
-
-//Light
-const light = new THREE.AmbientLight( 'white', 2 ); // soft white light
-const pointLight = new THREE.PointLight( "white", 600, 1000 );
-
-pointLight.position.set( 11, 3, 8);
-
-cube.position.x = 0
-
-scene.add(cube, pointLight,light);
-
-camera.position.z = 20;
-
-function resize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    camera.updateProjectionMatrix()
-}
-
-//const sphereSize = 0.2;
-//const pointLightHelper = new THREE.PointLightHelper( pointLight, sphereSize );
-//scene.add( pointLightHelper);
-
-window.addEventListener('resize', resize)
+const geometry = new THREE.SphereGeometry(5, 64, 32, 0, 6.3, 0, 6.3);
+const texture = new THREE.TextureLoader().load(
+  "/demo/img/cropped_image (3).png"
+);
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.repeat.set(6, 6);
+const material = new THREE.MeshLambertMaterial({ map: texture });
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
 
 function animate() {
-    requestAnimationFrame(animate);
-
-    //cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
-
-    controls.update();
-
-
-    renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  sphere.rotation.y += 0.01;
+  controls.update();
+  renderer.render(scene, camera);
 }
 
-//gsap.to(camera.position, {z: 5, ease: 'power2.out', duration:0.2, delay:1})
+window.addEventListener("resize", function () {
+  renderer.setSize(800, 600);
+  camera.aspect = 800 / 600;
+  camera.updateProjectionMatrix();
+  renderer.render(scene, camera);
+});
 
 animate();
+
+// listen for mouseenter and mouseleave events on the canvas
+const canvas = renderer.domElement;
+canvas.addEventListener("mouseenter", () => {
+  controls.enableZoom = true;
+});
+canvas.addEventListener("mouseleave", () => {
+  controls.enableZoom = false;
+});
